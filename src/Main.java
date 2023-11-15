329f435186df4d215f8b847bf4fbda63384b2d78
kajalinn <kajalinn@tlu.ee>
2023-11-15 18:55:31 +0200

    processor update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -9,13 +9,28 @@ import java.util.List;
 class Main {
     public static void main(String[] args) throws IOException {
 
+//        Path playerInputData = Path.of("resources/player_data.txt");
+//        Path matchInputData = Path.of("resources/match_data.txt");
+//
+//
+//        String playerData = Files.readString(playerInputData);
+//        String matchData = Files.readString(matchInputData);
 
-        Path playerInputData = Path.of("../resources/player_data.txt");
-        Path matchInputData = Path.of("../resources/match_data.txt");
+//        // Load player data
+//        String playerData = loadResourceAsString("../resources/player_data.txt");
+//
+//        // Load match data
+//        String matchData = loadResourceAsString("../resources/match_data.txt");
 
 
-        String playerData = Files.readString(playerInputData);
-        String matchData = Files.readString(matchInputData);
+
+
+        // Load player data
+        String playerData = loadResourceAsString("../resources/player_data.txt");
+
+        // Load match data
+        String matchData = loadResourceAsString("../resources/match_data.txt");
+
 
 
         PlayerTransactionProcessor playerProcessor = new PlayerTransactionProcessor();
@@ -28,6 +43,11 @@ class Main {
 
         TransactionProcessor transactionProcessor = new TransactionProcessor();
         transactionProcessor.processTransactions(playerTransactions, matchTransactions);
+    }
+
+    private static String loadResourceAsString(String resourceName) throws IOException {
+        Path resourcePath = Path.of(resourceName);
+        return Files.readString(resourcePath, StandardCharsets.UTF_8);
     }
 
 }
--- src/TransactionProcessor.java
+++ src/TransactionProcessor.java
@@ -1,23 +1,50 @@
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TransactionProcessor {
 
     public void processTransactions(List<PlayerTransaction> playerTransactions, List<MatchTransaction> matchTransactions) {
         // Compare and process both types of transactions
-        for (PlayerTransaction playerTransaction : playerTransactions) {
-            for (MatchTransaction matchTransaction : matchTransactions) {
-                if
-                (
-                        playerTransaction != null &&
-                        matchTransaction != null &&
-                        playerTransaction.getMatchID() != null &&
-                        playerTransaction.getMatchID().equals(matchTransaction.getMatchID())
-                ){
-                    System.out.println("Matching Pair: " + playerTransaction + " | " + matchTransaction);
-                    //further operations here
-                }
-            }
-        }
+
+        // more concise and lightweight solution
+        Map<String, MatchTransaction> matchTransactionMap = matchTransactions.stream()
+                .collect(Collectors.toMap(MatchTransaction::getMatchID, mt -> mt));
+
+        // Iterate over playerTransactions and check for matching pairs
+        playerTransactions.stream()
+                .filter(pt -> pt.getMatchID() != null && matchTransactionMap.containsKey(pt.getMatchID()))
+                .forEach(pt -> {
+                    MatchTransaction matchTransaction = matchTransactionMap.get(pt.getMatchID());
+                    System.out.println("Matching Pair: " + pt.bet_choice + " | " + matchTransaction.getMatchResult());
+                    // Perform further operations based on matching pair
+                });
+
+
+
+
+
+
+
+//        works but requires more resources and time with algorithm complexity O(n * m)
+//        for (PlayerTransaction playerTransaction : playerTransactions) {
+//            for (MatchTransaction matchTransaction : matchTransactions) {
+//                if
+//                (
+//                        playerTransaction != null &&
+//                        matchTransaction != null &&
+//                        playerTransaction.getMatchID() != null &&
+//                        playerTransaction.getMatchID().equals(matchTransaction.getMatchID())
+//                ){
+//                    System.out.println("Matching Pair: " + playerTransaction.getBetChoice() + " | " + matchTransaction.getMatchResult());
+//                    //further operations here
+//                }
+//            }
+//        }
+
     }
+
 }
